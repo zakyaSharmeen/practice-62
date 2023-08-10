e83d508c4e0071f5288065149f7649666f03b6e2
zakya sharmeen <111156443+zakyaSharmeen@users.noreply.github.com>
2023-08-10 13:58:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.menuToggle');
+    var navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
